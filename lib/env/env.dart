7cfae691c8580bd2a4f75303687540db418c9e71
Marius Oprea <marius.oprea03@gmail.com>
2024-03-03 10:28:42 +0200

    Setup envied

---
+++ lib/env/env.dart
@@ -0,0 +1,9 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'OPEN_AI_API_KEY')
+  static String openAiApiKey = _Env.openAiApiKey;
+}
